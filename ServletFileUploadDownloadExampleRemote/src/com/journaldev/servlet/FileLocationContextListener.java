b0086d3e2b1acafc7a2eac1d99104aa8741a4b12
Pasquale Amoruso <paky.nfs92@gmail.com>
2017-03-20 17:32:28 +0100

    ok folder tempfile.dir

--- ServletFileUploadDownloadExampleRemote/src/com/journaldev/servlet/FileLocationContextListener.java
+++ ServletFileUploadDownloadExampleRemote/src/com/journaldev/servlet/FileLocationContextListener.java
@@ -13,7 +13,7 @@ public class FileLocationContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
     	String rootPath = System.getProperty("catalina.home");
     	ServletContext ctx = servletContextEvent.getServletContext();
-    	String relativePath = ctx.getInitParameter("folder.dir");
+    	String relativePath = ctx.getInitParameter("tempfile.dir");
     	File file = new File(rootPath + File.separator + relativePath);
     	if(!file.exists()) file.mkdirs();
     	System.out.println("File Directory created to be used for storing files");
